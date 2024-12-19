7fb97246154f07738074ba9a11b72a7e0fb07d60
scheerp <patrick-scheer@web.de>
2024-12-19 15:35:37 +0100

    update requirements

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
+# exit on error
+set -o errexit
 
-# Installiere Chromium und Chromedriver über pip
-pip install chromedriver-autoinstaller
-pip install pyppeteer
+STORAGE_DIR=/opt/render/project/.render
+
+if [[ ! -d $STORAGE_DIR/chrome ]]; then
+  echo "...Downloading Chrome"
+  mkdir -p $STORAGE_DIR/chrome
+  cd $STORAGE_DIR/chrome
+  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+  rm ./google-chrome-stable_current_amd64.deb
+  cd $HOME/project/src # Make sure we return to where we were
+else
+  echo "...Using Chrome from cache"
+fi

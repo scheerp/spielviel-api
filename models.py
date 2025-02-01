56bebbe7fba61e729793a4d59a9216520866e174
scheerp <patrick-scheer@web.de>
2025-02-01 20:06:34 +0100

    fix alembic

--- models.py
+++ models.py
@@ -147,7 +147,7 @@ class Game(Base):
 
 class User(Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     username = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
